0c84bd81376db8f49921f64f58870553100ebbea
Long Tran <v.long128@gmail.com>
2015-09-03 18:48:36 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sendvoteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sendvoteApp
+ */
+angular.module('sendvoteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sendvoteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sendvoteApp
+ */
+angular.module('sendvoteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
