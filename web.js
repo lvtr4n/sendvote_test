ae5ed005bd1c5518635e938f841c3cd1ef6a80fc
Long Tran <v.long128@gmail.com>
2015-09-03 18:57:43 -0700

    init

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 var morgan = require("morgan");
-app.user(morgan('combined'));
+app.use(morgan('combined'));
 
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
